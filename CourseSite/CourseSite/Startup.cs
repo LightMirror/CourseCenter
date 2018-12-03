5e5c9773fca8efa0765cf86ef69749a10c63d460
hadyaskalany <45493306+hadyaskalany@users.noreply.github.com>
2018-12-03 17:27:19 +0200

    First

---
+++ CourseSite/CourseSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseSite.Startup))]
+namespace CourseSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
