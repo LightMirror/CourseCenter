b95d2633dd8f4faf52fe5d04bc40578394f856db
eng-abdulrheem <eng.abdulrheem@gmail.com>
2018-12-12 22:15:30 +0200

    resources by abdulrheem

--- CourseSite/CourseSite/Controllers/HomeController.cs
+++ CourseSite/CourseSite/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -30,5 +32,20 @@ namespace CourseSite.Controllers
 
             return View();
         }
+        public ActionResult ChangeLang(string lang)
+        {
+            if (lang != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = lang;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+
     }
 }
