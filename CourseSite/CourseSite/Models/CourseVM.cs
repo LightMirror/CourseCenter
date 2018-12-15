36f4ef313e9181337cd50adf253c0f78620d53b8
AmrMohammedHassan <amromohammed94@gmail.com>
2018-12-16 01:20:02 +0200

    coursecontrol

---
+++ CourseSite/CourseSite/Models/CourseVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseSite.Models
+{
+    public class CourseVM
+    {
+        public string CourseName { get; set; }
+        public string CourseImg { get; set; }
+        public string CourseObjective { get; set; }
+        public string CourseSumery { get; set; }
+    }
+}
