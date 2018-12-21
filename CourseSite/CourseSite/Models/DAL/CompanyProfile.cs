ad9558f4a28682ffb1bdc54ba6c5088b4fe93779
eng-abdulrheem <eng.abdulrheem@gmail.com>
2018-12-21 17:21:38 +0200

    djfykuigiohjpk

--- CourseSite/CourseSite/Models/DAL/CompanyProfile.cs
+++ CourseSite/CourseSite/Models/DAL/CompanyProfile.cs
@@ -88,7 +88,5 @@ namespace CourseSite.Models.DAL
 
         [Display(Name = "Arabic HeadLine")]
         public string CompanyHeadLineAra { get; set; }
-        public int ID { get; set; }
     }
-}
 }
