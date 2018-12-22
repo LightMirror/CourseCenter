10ccee54c96e3775f33c1955458bf185e403dd74
AmrMohammedHassan <amromohammed94@gmail.com>
2018-12-22 19:19:17 +0200

    breadcrumb

--- CourseSite/CourseSite/App_Start/BundleConfig.cs
+++ CourseSite/CourseSite/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace CourseSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/breadcrumb.css"));
         }
     }
 }
