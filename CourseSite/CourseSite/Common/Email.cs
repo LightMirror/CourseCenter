75d845b352dc05b720fab997a15fc9d019b1df40
hadyaskalany <45493306+hadyaskalany@users.noreply.github.com>
2019-01-14 13:32:25 +0200

    Revert "hady"
    This reverts commit f5708505c34193f8463613e2d9148098ce63953f.

--- CourseSite/CourseSite/Common/Email.cs
+++ CourseSite/CourseSite/Common/Email.cs
@@ -15,13 +15,9 @@ namespace CourseSite.Common
         {
             try
             {
-
+                
                 var Senderemail = new MailAddress("lightmirror.eg@gmail.com");
                 var Password = "P@ssw0rdIATLC";
-
-                //var Senderemail = new MailAddress("hady_askalany@yahoo.com");
-                //var Password = "P@ss4raya";
-
                 // var Reciveremail = new MailAddress(recevir);
 
                 var Sub = subject;
@@ -33,10 +29,10 @@ namespace CourseSite.Common
                 var stmp = new SmtpClient
                 {
                     Host = "smtp.gmail.com",
-                    Port = 465, // use this port with Enabe SSL
+                    Port = 25, // use this port with Enabe SSL
                     EnableSsl = true,
                     DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
+                    UseDefaultCredentials = true,
                     Credentials = new System.Net.NetworkCredential(Senderemail.Address, Password)
                 };
 
@@ -71,39 +67,9 @@ namespace CourseSite.Common
             catch (Exception ex)
             {
                 throw ex;
+                return false;
             }
-        }
-
-        public static bool SendEmail(string recevir, string subject, string body, System.Net.Mail.Attachment attachment = null) //hady askalany 13-01-2019
-        {
-            try
-            {
-                var Senderemail = new MailAddress("lightmirror.eg@gmail.com");
-                var Password = "P@ssw0rdIATLC";
-
-                //var Senderemail = new MailAddress("hady_askalany@yahoo.com");
-                //var Password = "P@ss4raya";
-
-                SmtpClient client = new SmtpClient("smtp.gmail.com", 465);
-                client.EnableSsl = true;
-                client.Timeout = 3000000; 
-                client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                client.UseDefaultCredentials = true;
-                client.Credentials = new System.Net.NetworkCredential("lightmirror.eg@gmail.com", Password);
-                MailMessage message = new MailMessage();
-                message.To.Add(recevir);
-                message.From = new MailAddress("lightmirror.eg@gmail.com");
-                message.Subject = subject;
-                message.Body = body;
-                client.ServicePoint.MaxIdleTime = System.Threading.Timeout.Infinite; 
-                client.Send(message);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
+        }       
 
         public static bool SendWhatsapp(string to,string msg)
         {
